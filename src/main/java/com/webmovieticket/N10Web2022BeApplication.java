55d74e5589651945a7ecb497db3d2fdc73a4ee34
duykien1310 <kien.nguyenduy.vnu@gmail.com>
2022-10-18 22:00:45 +0700

    init commit

---
+++ src/main/java/com/webmovieticket/N10Web2022BeApplication.java
@@ -0,0 +1,13 @@
+package com.webmovieticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N10Web2022BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N10Web2022BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webmovieticket/N10Web2022BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmovieticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N10Web2022BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
