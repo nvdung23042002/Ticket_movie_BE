d8da60e49e4ce2eb294008c82a37d472e6121958
duykien1310 <kien.nguyenduy.vnu@gmail.com>
2022-12-07 23:11:22 +0700

    update

---
+++ src/main/java/com/webmovieticket/controller/RoomController.java
@@ -0,0 +1,22 @@
+package com.webmovieticket.controller;
+
+import com.webmovieticket.models.CinemaRoom;
+import com.webmovieticket.repository.CinemaRoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping(path = "test/room")
+public class RoomController {
+    @Autowired
+    private CinemaRoomRepository cinemaRoomRepository;
+
+    public List<CinemaRoom> findAll() {
+        return cinemaRoomRepository.findAll();
+    }
+}
